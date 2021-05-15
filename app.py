acf386b7bec00a6ed31145dabedf3cf39d65aafd
Onimaru Shohei <70475483+s-onineko@users.noreply.github.com>
2021-05-16 04:31:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ run_pred = st.checkbox("AutoMLによる予測を実行")
 
 if run_pred == True :
     save_path = 'agModels-predictClass'  # specifies folder to store trained models
-    predictor = TabularPredictor(label=label, path=save_path, presets='best_quality', time_limit=600).fit(df_train)
+    predictor = TabularPredictor(label=label, path=save_path).fit(df_train, presets='best_quality', time_limit=600)
     y_test = df_test[label]  # values to predict
     test_data_nolab = df_test.drop(columns=[label])  # delete label column to prove we're not cheating
     predictor = TabularPredictor.load(save_path)  # unnecessary, just demonstrates how to load previously-trained predictor from file
