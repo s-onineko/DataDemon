aa00f6bc71abcf9e29d341a83e8ab05e359f1005
Onimaru Shohei <70475483+s-onineko@users.noreply.github.com>
2021-05-15 17:36:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -114,6 +114,7 @@ st.dataframe(df.head())
 # 前処理
 target = st.selectbox("目的変数を選択してください",list(df.columns))
 features = st.multiselect("説明変数を選択してください",list(df.columns))
+df_list = list(target)
 df_list = target + features
 st.write(df_list)
 df = df.loc[:,df_list]
